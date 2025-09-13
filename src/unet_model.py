ce3d7442bbe1fca52c7ed7108a9b7e390531969c
David Lee <doctor.dlee@gmail.com>
2025-09-12 23:58:35 -0700

    comma

--- src/unet_model.py
+++ src/unet_model.py
@@ -10,7 +10,7 @@ class ConvBlock(nn.Module):
         super().__init__()
         self.block = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size = 3, padding = 1),
-            nn.BatchNorm2d(out_channels)
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(),
             nn.Conv2d(out_channels, out_channels, kernel_size = 3, padding = 1),
             nn.BatchNorm2d(out_channels),
