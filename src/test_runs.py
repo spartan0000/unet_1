a12d42c91c58d02aaa82c86dcc1c88a74870422e
David Lee <doctor.dlee@gmail.com>
2025-09-20 03:56:37 -0700

    minor changes

--- src/test_runs.py
+++ src/test_runs.py
@@ -41,46 +41,50 @@ noisy_train_files, noisy_test_files = train_test_split_foggy_noisy(clear_dir, no
 noisy_train_loader, noisy_test_loader, noisy_train_subset_loader, noisy_test_subset_loader = build_foggy_noisy_data_loader(clear_dir, noisy_dir, noisy_train_files, noisy_test_files, 500, 100)
 
 #subset test run
-
-
+model_lite = Unet_lite(3,3).to(device)
+loss_fn = nn.MSELoss()
+optimizer = torch.optim.Adam(params = model_lite.parameters(), lr = lr)
 
 def main():
 
-    start_time = timer()
-    training_loss = 0.0
+    for epoch in range(n_epochs):
+        start_time = timer()
+        training_loss = 0.0
 
-    model_lite = Unet_lite(3,3).to(device)
+        
+        model_lite.train()
 
-    loss_fn = nn.MSELoss()
-    optimizer = torch.optim.Adam(params = model_lite.parameters(), lr = lr)
 
-    for images, targets in noisy_train_subset_loader:
-        images, targets = images.to(device), targets.to(device)
+        
 
-        outputs = model_lite(images)
-        loss = loss_fn(outputs, targets)
-
-        optimizer.zero_grad()
-
-        loss.backward()
-
-        optimizer.step()
-
-    training_loss += loss.item() * images.size(0)
-    
-    training_loss /= len(noisy_train_subset_loader.dataset)
-    test_loss = 0.0
-    model_lite.eval()
-    with torch.inference_mode():
-        for images, targets in noisy_test_subset_loader:
+        for images, targets in noisy_train_subset_loader:
             images, targets = images.to(device), targets.to(device)
-            test_outputs = model_lite(images)
-            loss = loss_fn(test_outputs, targets)
-            test_loss += loss.item() * images.size(0)
-        test_loss /= len(noisy_test_subset_loader.dataset)
-    end_time = timer()
-    print(f' Training loss: {training_loss:.4f} | Test loss: {test_loss:.4f}')
-    print(f'Total time: {end_time - start_time:.4f} seconds on {device}')
+
+            outputs = model_lite(images)
+            loss = loss_fn(outputs, targets)
+
+            optimizer.zero_grad()
+
+            loss.backward()
+
+            optimizer.step()
+
+        training_loss += loss.item() * images.size(0)
+        
+        training_loss /= len(noisy_train_subset_loader.dataset)
+        test_loss = 0.0
+        model_lite.eval()
+        with torch.inference_mode():
+            for images, targets in noisy_test_subset_loader:
+                images, targets = images.to(device), targets.to(device)
+                test_outputs = model_lite(images)
+                loss = loss_fn(test_outputs, targets)
+                test_loss += loss.item() * images.size(0)
+            test_loss /= len(noisy_test_subset_loader.dataset)
+        end_time = timer()
+        print(f' {epoch + 1} / {n_epochs} - Training loss: {training_loss:.4f} | Test loss: {test_loss:.4f}')
+        print(f'Total time: {end_time - start_time:.4f} seconds on {device}')
+
 
 if __name__ == '__main__':
     main()
